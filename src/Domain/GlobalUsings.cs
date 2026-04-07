8497873deebf11ef52027b37c74a5a0b3b72dd33
Ricardo Chavez <rickardo.chavez@gmail.com>
2026-04-07 13:51:11 -0500

    Clean up unused global usings in Domain layer

--- src/Domain/GlobalUsings.cs
+++ src/Domain/GlobalUsings.cs
@@ -1,6 +1,2 @@
 ﻿global using MainApi.Domain.Common;
 global using MainApi.Domain.Entities;
-// global using MainApi.Domain.Enums;
-// global using MainApi.Domain.Events;
-// global using MainApi.Domain.Exceptions;
-// global using MainApi.Domain.ValueObjects;
